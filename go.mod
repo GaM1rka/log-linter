be9e2dfe93b5e9af964568bae808f398fcf5a1c1
amir <am.gabdullin@mail.ru>
2026-02-14 00:08:33 +0300

    golangci v2 module plugin registration

--- go.mod
+++ go.mod
@@ -1,9 +1,17 @@
 module github.com/GaM1rka/log-linter
 
-go 1.22
+go 1.23.0
 
-require golang.org/x/tools v0.17.0
+toolchain go1.24.4
 
-require golang.org/x/mod v0.14.0 // indirect
+require (
+	github.com/golangci/plugin-module-register v0.1.2
+	golang.org/x/tools v0.32.0
+)
+
+require (
+	golang.org/x/mod v0.24.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+)
 
 replace golang.org/x/mod v0.33.0 => golang.org/x/mod v0.14.0
--- plugin/golangci/plugin.go
+++ plugin/golangci/plugin.go
@@ -2,11 +2,25 @@ package golangci
 
 import (
 	"github.com/GaM1rka/log-linter/pkg/analyzer"
+	"github.com/golangci/plugin-module-register/register"
 	"golang.org/x/tools/go/analysis"
 )
 
-func New(_ any) ([]*analysis.Analyzer, error) {
-	return []*analysis.Analyzer{
-		analyzer.Analyzer,
-	}, nil
+type Plugin struct{}
+
+func init() {
+	register.Plugin("loglinter", New)
+}
+
+func New(_ any) (register.LinterPlugin, error) {
+	return &Plugin{}, nil
+}
+
+func (p *Plugin) BuildAnalyzers() ([]*analysis.Analyzer, error) {
+	return []*analysis.Analyzer{analyzer.Analyzer}, nil
+}
+
+func (p *Plugin) GetLoadMode() string {
+	// тебе нужен types info для zap (pass.TypesInfo), значит TypesInfo mode
+	return register.LoadModeTypesInfo
 }
